04d98abdf615642e19913a048ccfa5140cccc428
Kimathi isaac <Isaackimathi@gmail.com>
2023-05-21 23:42:33 +0300

    c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,15 +6,14 @@
  */
 int main(void)
 {
-	int i;
+	int i = 97;
 
-	i = 97;
 	while (i <= 122)
 	{
-		if (i == 113 || i == 101)
+		if (i == 101 || i == 113)
 		{
-			continue;
 			i++;
+			continue;
 		}
 		putchar(i);
 		i++;
