633c83e68c1dd99916ff5075ff0667bc546e9c03
Kimathi isaac <Isaackimathi@gmail.com>
2023-05-22 01:00:04 +0300

    c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet, followed by a new line
+ * Description - alphabet is in lowercase and reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n < 97)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
