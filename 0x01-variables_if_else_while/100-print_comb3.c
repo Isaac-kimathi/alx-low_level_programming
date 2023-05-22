314ba99f9685443d8679f44237192220ba320fb0
Kimathi isaac <Isaackimathi@gmail.com>
2023-05-23 01:39:34 +0300

    c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two numbers
+ * Description - two digit are different, in an ascending order
+ *               and smallest cominations
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+	int d;
+
+	for (n = 48; n <= 57; n++)
+	{
+	for (d = 49; d <= 57; d++)
+	{
+		if (n > d)
+			continue;
+		if (n == d)
+			continue;
+		putchar(n);
+		putchar(d);
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
