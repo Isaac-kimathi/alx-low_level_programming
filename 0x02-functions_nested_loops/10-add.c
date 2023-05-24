fc3a5e90ba00fd02d830f6f8d6fe03503457e847
Kimathi isaac <Isaackimathi@gmail.com>
2023-05-24 19:56:01 +0300

    c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - function that adds two integers
+ * @k: parameter1
+ * @l: parameter2
+ * Return: result
+ */
+int add(int k, int l)
+{
+	int res;
+
+	res = k + l;
+
+	return (res);
+}
