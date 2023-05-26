f18415483436db27e8a763083ebdd5112bd4310f
Kimathi isaac <Isaackimathi@gmail.com>
2023-05-26 13:28:45 +0300

    c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,16 +11,19 @@ void print_diagonal(int n)
 {
 	int x, y;
 
+	if (n > 0)
+	{
 	for (x = 0; x < n; x++)
 	{
 		for (y = 0; y < x; y++)
 		{
 			_putchar(32);
 		}
-		if (x <= 0)
-		_putchar('\n');
 
 		_putchar(92);
 		_putchar('\n');
 	}
+	}
+	else
+		_putchar('\n');
 }
