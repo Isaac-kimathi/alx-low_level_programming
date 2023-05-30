60340476474b4fd54697e3a5ac74a62edbb7c3d7
Kimathi isaac <Isaackimathi@gmail.com>
2023-05-31 01:32:41 +0300

    c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,14 +1,14 @@
 #include "main.h"
  /**
- * print_array - prints n elements of an array of integers, 
- *  followed by a new line
- * description: Numbers must be separated by comma, followed by a space, and
- *    displayed in the same order as they are stored in the array
- * @n: number of elements of the array to be printed
- * @a: Array of integers
- * Return: void
- */
- void print_array(int *a, int n)
+  * print_array - prints n elements of an array of integers,
+  * followed by a new line
+  * description - numbers must be separated by comma, followed by a space, and
+  * displayed in the same order as they are stored in the array
+  * @n: number of elements of the array to be printed
+  * @a: Array of integers
+  * Return: void
+  */
+void print_array(int *a, int n);
 {
 	int x;
 
