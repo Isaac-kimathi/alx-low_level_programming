bacc89caf3801ebb00590fe7c1ff9ac1a96292b0
Kimathi isaac <Isaackimathi@gmail.com>
2023-05-31 01:35:16 +0300

    c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,13 +2,14 @@
  /**
   * print_array - prints n elements of an array of integers,
   * followed by a new line
+  *
   * description - numbers must be separated by comma, followed by a space, and
   * displayed in the same order as they are stored in the array
   * @n: number of elements of the array to be printed
   * @a: Array of integers
   * Return: void
   */
-void print_array(int *a, int n);
+void print_array(int *a, int n)
 {
 	int x;
 
