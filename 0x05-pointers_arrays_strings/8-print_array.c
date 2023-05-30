dee823f37ed109548c5b519a92bdec2e7a81b045
Kimathi isaac <Isaackimathi@gmail.com>
2023-05-31 01:25:26 +0300

    c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+ /**
+ * print_array - prints n elements of an array of integers, 
+ *  followed by a new line
+ * description: Numbers must be separated by comma, followed by a space, and
+ *    displayed in the same order as they are stored in the array
+ * @n: number of elements of the array to be printed
+ * @a: Array of integers
+ * Return: void
+ */
+ void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		if (x != (n - 1))
+			printf("%d, ", a[x]);
+		else
+			printf("%d", a[x]);
+	}
+	printf("\n");
+}
