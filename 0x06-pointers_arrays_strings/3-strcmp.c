97271abfa98059cb5fe93695ffb52a8fe54620aa
Kimathi isaac <Isaackimathi@gmail.com>
2023-06-03 01:59:43 +0300

    c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcmp - function that compares two strings
+ * @s1: string pointer 1
+ * @s2: string pointer 2
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int q;
+
+	q = strcmp(s1, s2);
+	return (q);
+}
