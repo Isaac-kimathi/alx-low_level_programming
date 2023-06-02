056bbf58ae423f23313a5ab64746c2ed1fa3c044
Kimathi isaac <Isaackimathi@gmail.com>
2023-06-03 00:43:53 +0300

    c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,18 +5,22 @@
  *    src does not need to be null-terminated if it contains n or more bytes
  * @dest: pointer to destination input
  * @src: pointer to source input
- * @n: most no of bytes from &src
+ * @n: most no of bytes from address of src
  * Return: &dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int x, y;
+	int x = 0;
+	int y;
 
-	x = 0;
 	while (dest[x])
 		x++;
-	for (y = 0; y < n && src[y] != '\0'; y++)
-	       dest[x + y] = src[y];
+	y = 0;
+	while (y < n && src[y] != '\0')
+	{
+		dest[x + y] = src[y];
+		y++;
+	}
 	dest[x + y] = '\0';
 
 	return (dest);
