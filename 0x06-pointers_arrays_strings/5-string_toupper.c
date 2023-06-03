fb24621d99e8246bfab0e20420f6f811311e974d
Kimathi isaac <Isaackimathi@gmail.com>
2023-06-03 03:13:44 +0300

    c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *string_toupper - function that changes all lowercase letters of a 
+ *   string to uppercase
+ * @b: string parameter
+ * Return: string
+ */
+char *string_toupper(char *b)
+{
+	int g;
+
+	for (g = 0; b[g] != '\0'; g++)
+	{
+		if (b[g] >= 97 && b[g] <= 122)
+		{
+			b[g] = b[g] - 32;
+		}
+	}
+	return (b);
+}
