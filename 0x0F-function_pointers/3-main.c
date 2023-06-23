de9e7864de75f451dfe86cde644f9d4fed54b6a9
Kimathi isaac <Isaackimathi@gmail.com>
2023-06-24 02:31:18 +0300

    update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int (*oprt)(int, int);
+	int (*op_func)(int, int), a, b;
 
 	if (argc != 4)
 	{
@@ -17,14 +17,17 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	oprt = get_op_func(argv[2]);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
-	if (!oprt)
-	{
-		printf("Error\n");
-		exit(99);
-	}
+	op_func = get_op_func(argv[2]);
 
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	if (!op_func)
+		printf("Error\n"), exit(99);
+
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+		printf("Error\n"), exit(100);
+
+	printf("%d\n", op_func(a, b));
 	return (0);
 }
