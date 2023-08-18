ef5bc598170c23d48c419bb004935b3409f55b54
Kimathi isaac <Isaackimathi@gmail.com>
2023-08-18 15:21:37 +0300

    C - Doubly linked lists

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -3,6 +3,7 @@
 /**
  * delete_dnodeint_at_index - deletes the node at index index of
  * a dlistint_t linked list
+ * @head: beginning of doubly liked list
  * @index: index of the node that should be deleted
  * Return: 1 if it succeeded, -1 if it failed
  */
@@ -12,7 +13,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int x;
 
 	if (head == NULL || *head == NULL)
-		return(-1);
+		return (-1);
 	now = *head;
 	if (index == 0)
 	{
