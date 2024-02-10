1542664db99b671d94a3206ad9d8e7572b591fb8
Kimathi isaac <Isaackimathi@gmail.com>
2024-02-10 20:26:11 +0300

    Exponential search algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,40 +1,59 @@
 #include "search_algos.h"
 
 /**
- * binary_search - searches for a value in a sorted array of integers using the Binary search algorithm
+ * search_recursively - searches for a value in an array of
+ *  integers using the Binary search algorithm recursively
  * @array: array to search
- * @size: size of an array
+ * @size: size of array
  * @value: value to search for
- * Return: index containing `value`, or -1 if `value` not found or `array` is NULL
+ * Return: index of number or -1 incase of failure
+ */
+int search_recursively(int *array, size_t size, int value)
+{
+	size_t mddl = size / 2;
+	size_t x;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (x = 0; x < size; x++)
+		printf("%s %d", (x == 0) ? ":" : ",", array[x]);
+
+	printf("\n");
+
+	if (mddl && size % 2 == 0)
+		mddl--;
+
+	if (value == array[mddl])
+		return ((int)mddl);
+
+	if (value < array[mddl])
+		return (search_recursively(array, mddl, value));
+
+	mddl++;
+
+	return (search_recursively(array + mddl, size - mddl, value) + mddl);
+}
+
+/**
+ * binary_search - calls to binary_search to return
+ * the index of the number
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int lwr = 0, mddl, hghr = size - 1;
-	int i;
+	int idx;
 
-	if (array == NULL)
-		return(-1);
+	idx = search_recursively(array, size, value);
 
-	while (lwr <= hghr)
-	{
-		printf("searching in array: ");
-		for (i = lwr; i <= hghr; i++)
-		{
-			if (i == hghr)
-				printf("%d\n", array[i]);
-			else
-				printf("%d, ", array[i])
-		}
+	if (idx >= 0 && array[idx] != value)
+		return (-1);
 
-		mddl = (lwr + hghr) / 2;
-
-		if (array[mddl] < value)
-			lwr = mddl + 1;
-		else if (array[mddl] > value)
-			hghr = mddl - 1;
-		else
-			return(mddl);
-	}
-
-	return(-1);
+	return (idx);
 }
